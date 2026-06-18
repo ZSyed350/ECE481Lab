c76b134c9fe6f017f16b78f4c26dc6ba143342ee
Amir Hamadache <ahamadac@uwaterloo.ca>
2026-06-18 16:19:36 -0400

    effective ball position

--- ECE481Lab.ino
+++ ECE481Lab.ino
@@ -13,8 +13,10 @@ volatile int   pos_counts = 0;       // ball position
 volatile float BallPos    = 0.0;     // ball position in m
 volatile int          Tms = 10;     // 100 ms interrupt interval
 float voltage = 0.0;
-const float m = -0.0213;  // slope for radian conversion
-const float offset = 120.24;  // offset for radian conversion
+const float m_motor = -0.0213;      // slope for radian conversion
+const float offset_motor = 120.24;  // offset for radian conversion
+const float m_ball = 0.0000245;       // slope for ball effective position in meters
+const float offset_ball = -0.0161;  // offset for ball effective position in meters
 const float P = -30;  // Gain
 int32_t T = 5000;   // Period for step function
 float RefServoAng = 0.0; // target radian from input of step function
@@ -47,9 +49,9 @@ void setup() {
 
 // ================== Loop ==================
 void loop() {
-  RefServoAng = HIGH_REF;
+  // RefServoAng = HIGH_REF;
   delay(2000);
-  RefServoAng = LOW_REF;
+  // RefServoAng = LOW_REF;
   delay(2000);
 }
 
@@ -69,22 +71,29 @@ void interval_control_code(void) {
     RefServoAng = -0.7;
 
   // Control
-  ServoAng = (m * angle_counts + offset) * 3.14159/180; // rad
+  ServoAng = (m_motor * angle_counts + offset_motor) * 3.14159/180; // rad
   float error = RefServoAng - ServoAng;
   voltage = ctrlr.control(error);
   if (ServoAng > 0.8 || ServoAng < -0.8)
     voltage = 0.0;
   setMotorVoltage(voltage);
 
-  digitalWrite(A5,HIGH);   // A5 can be used to measure cycle time of ISR using an oscilloscope by connecting the scope to the Arduino Box Motor Leads
-  
+  digitalWrite(A5,HIGH);   // motor angle sensor - A5 can be used to measure cycle time of ISR using an oscilloscope by connecting the scope to the Arduino Box Motor Leads
+
+  // Effective Ball Position (in meters)
+  float effBallPos = m_ball * pos_counts + offset_ball;
+
   // Timer
   timeCounter += 10;
+
+  // Printing
   Serial.print(timeCounter);
   Serial.print(",");
   Serial.print(RefServoAng);
   Serial.print(",");
-  Serial.println(ServoAng);
+  Serial.print(ServoAng);
+  Serial.print(",");
+  Serial.println(effBallPos);
 
   digitalWrite(A5,LOW);    // A5 can be used to measure cycle time of ISR using an oscilloscope by connecting the scope to the Arduino Box Motor Leads
 }
